1c00e557e0e10e8d20dee97d2494bf6fd6a96395
fvrbloom <dilnaz1327@gmail.com>
2023-04-28 16:39:14 +0600

    fit out

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(147, 168, 253,50);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+const double defaultPadding = 10.0;
+const double defaultMargin = 10.0;
